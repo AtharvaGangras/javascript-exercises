9784cfe9bd24b8649868d57154ba61d72168c970
Atharva Gangras <acgangras@gmail.com>
2024-04-09 07:00:09 +0530

    solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farhenheit) {
+  let ans = (farhenheit-32)*5/9;
+  ans = Math.round(ans*10)/10;
+  return ans;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let ans = celcius*9/5 +32;
+  ans = ans.toFixed(1);
+  return Number(ans);
 };
 
 // Do not edit below this line
