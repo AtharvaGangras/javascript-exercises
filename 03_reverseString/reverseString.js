a161edeace7ecbc5aba26f8fefac6bb9bd46054c
Atharva Gangras <acgangras@gmail.com>
2024-04-09 06:25:28 +0530

    solve 03 04

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    // str = str.split("");
+    // str.reverse();
+    // str = str.join("");
+    // return str;
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...toBeRemoved) {
+    // let indexOfRemoveElement = arr.indexOf(value);
+    // arr.splice(indexOfRemoveElement,1);
+    // return arr;
+
+    //my implementation
+    // for (let remove of toBeRemoved) {
+    //     while (true) {
+    //         let indexOfRemoveElement = arr.indexOf(remove);
+    //         if(indexOfRemoveElement === -1) break; 
+    //         arr.splice(indexOfRemoveElement, 1);
+    //     }
+    // }
+    // return arr;
+
+    return arr.filter(value=> !toBeRemoved.includes(value));
 
 };
 
