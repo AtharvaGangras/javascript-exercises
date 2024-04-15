33b4bad5e5a556f96789f45c3ee01b077e8275f8
Atharva Gangras <acgangras@gmail.com>
2024-04-15 07:54:10 +0530

    solve 08 09

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((total,element)=>total+element,0)
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((total,element)=>total*element,1)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let ans = 1;
+  while(num>1){ ans*=num; num--;}
+  return ans;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    splitString = string.split('');
+    splitString = splitString.filter(element=>(element>='a' && element<='z')||(element>='0' &&element<='9'));
 
+    let i = 0;
+    let j = splitString.length-1
+    while(i<j){
+        if(splitString[i] !== splitString[j]) return false;
+        i++;
+        j--;
+    }
+    return true;
 };
 
 // Do not edit below this line
